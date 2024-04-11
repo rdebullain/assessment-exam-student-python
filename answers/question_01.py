055ef7d055253d6bd3162dd169033ca1f4395070
Rida de Bullaín <153687074+rdebullain@users.noreply.github.com>
2024-04-11 17:49:44 -0400

    versioncontrol

--- answers/question_01.py
+++ answers/question_01.py
@@ -20,7 +20,11 @@ Notes:
 
 """
 def equal_slices(total_slices, no_recipients, slices_each):
-    if total_slices - (no_recipients * slices_each) >= 0:
+    if no_recipients == 0:
+        x = True
+        y = total_slices
+        return (x, y)
+    elif total_slices - (no_recipients * slices_each) >= 0:
         x = True
         y = (total_slices % (no_recipients * slices_each))
         return (x, y)
@@ -28,13 +32,11 @@ def equal_slices(total_slices, no_recipients, slices_each):
         x = False
         y = None
         return (x, y)
-    elif no_recipients == 0:
-        x = True
-        y = total_slices
-        return (x, y)
+    
 
 print(equal_slices(11, 5, 2))
 print(equal_slices(11, 5, 3))
 print(equal_slices(8, 3, 2))
 print(equal_slices(8, 3, 3))
 print(equal_slices(24, 12, 2))
+print(equal_slices(8, 0, 2))
